4e4285f7d04600f3a37b958e82ab69e3ddf0c3eb
Sergiu Popa <popa.sergiu920@gmail.com>
2020-04-15 19:36:21 +0300

    file collection

--- src/App/Http/Controllers/File/Index.php
+++ src/App/Http/Controllers/File/Index.php
@@ -5,7 +5,7 @@ namespace LaravelMerax\FileServer\App\Http\Controllers\File;
 
 
 
-use LaravelMerax\FileServer\App\Http\Resources\FilesCollection;
+use LaravelMerax\FileServer\App\Http\Resources\Collection;
 use LaravelMerax\FileServer\App\Models\File;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
@@ -14,7 +14,7 @@ class Index extends Controller
 {
     public function __invoke(Request $request)
     {
-        return new FilesCollection(
+        return new Collection(
             File::visible()
                 ->forUser($request->user())
                 ->get()
